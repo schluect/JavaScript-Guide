10ec24c4e4f711b7a1a0c06421fb351ece6611bd
trsch2012@live.com <trsch2012@live.com>
2014-04-07 22:42:00 -0500

    int commit

---
+++ JavaScript/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JavaScript.Startup))]
+namespace JavaScript
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
